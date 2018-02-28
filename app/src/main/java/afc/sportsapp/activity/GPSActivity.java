47d287efe5859ba3a35371acb191cc63c49eeeb3
BenjaminSorianoDev <soriano.benjamin.pro@gmail.com>
2018-02-28 14:16:15 +0100

    adding chrono

--- app/src/main/java/afc/sportsapp/activity/GPSActivity.java
+++ app/src/main/java/afc/sportsapp/activity/GPSActivity.java
@@ -1,7 +1,15 @@
 package afc.sportsapp.activity;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,20 +20,78 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import afc.sportsapp.R;
 
-public class GPSActivity extends FragmentActivity implements OnMapReadyCallback {
+public class GPSActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Chronometer chronometer;
+    private Button start;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_workout:
+                    startActivity(new Intent(GPSActivity.this, WorkoutActivity.class));
+                    return true;
+                case R.id.navigation_program:
+                    startActivity(new Intent(GPSActivity.this, ProgramActivity.class));
+                    return true;
+                case R.id.navigation_challenge:
+                    startActivity(new Intent(GPSActivity.this, ChallengeActivity.class));
+                    return true;
+                case R.id.navigation_stats:
+                    startActivity(new Intent(GPSActivity.this, StatsActivity.class));
+                    return true;
+                case R.id.navigation_profile:
+                    startActivity(new Intent(GPSActivity.this, ProfileActivity.class));
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gps);
+
+        this.chronometer = (Chronometer) findViewById(R.id.chronometer);
+        this.start = (Button) findViewById(R.id.begin_chrono);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        //beginButtonPress();
+        start.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                if(start.getText() == getString(R.string.action_start)) {
+                    chronometer.start();
+                    start.setText(getString(R.string.action_stop));
+                    return;
+                }
+                if(start.getText() == getString(R.string.action_stop)) {
+                    chronometer.stop();
+                    start.setText(getString(R.string.action_start));
+                    return;
+                }
+            }
+        });
     }
 
+    /**private void beginButtonPress(){
+        Button b = (Button) findViewById(R.id.begin_chrono);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chronometer.start();
+            }
+        });
+    }**/
 
     /**
      * Manipulates the map once available.
